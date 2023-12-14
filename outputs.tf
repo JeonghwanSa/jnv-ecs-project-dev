08c71e3b106f68b5ea7d40889a97fd1571a83a61
JeonghwanSa <jeonghwansa@jobis.co>
2023-12-14 11:36:45 +0900

    fix output value

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "service_name" {
   value = aws_ecs_service.jnv_ecs_service.name
 }
 output "secret_arn" {
-  value = ""
+  value = "arn:aws:secretsmanager:ap-northeast-2:185236431346:secret:no-use"
 }
