d09b55088225a1a8c7a87359cf9f31c5173f8228
JeonghwanSa <jeonghwansa@jobis.co>
2023-12-20 19:32:42 +0900

    add service sg id output

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,7 @@ output "service_name" {
 output "secret_arn" {
   value = aws_secretsmanager_secret.jnv_ecs_service_secrets.arn
 }
+
+output "ecs_service_sg_id" {
+  value = aws_security_group.jnv_ecs_service_sg.id
+}
